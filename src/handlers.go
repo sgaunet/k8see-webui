9f0e5e7bfb1281d7b23241d5d4588121ca3d218b
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2022-02-13 22:16:05 +0100

    Add exportedTime field + rename fields

--- src/handlers.go
+++ src/handlers.go
@@ -9,17 +9,18 @@ import (
 )
 
 type rowK8sevents struct {
-	K8seventTs      time.Time
-	K8seventFirstTs time.Time
-	K8seventName    string
-	K8seventReason  string
-	K8seventType    string
-	K8seventMessage string
+	ExportedTime time.Time
+	EventTime    time.Time
+	FirstTime    time.Time
+	Name         string
+	Reason       string
+	Type         string
+	Message      string
 }
 
 func (s *appServer) GetMinDate() (time.Time, error) {
 	var dbegin time.Time
-	rqt := "select min(firstEventTs) from k8sevents;"
+	rqt := "select min(firstTime) from k8sevents;"
 	rows, err := s.db.Query(rqt)
 	if err != nil {
 		return dbegin, err
@@ -34,24 +35,6 @@ func (s *appServer) GetMinDate() (time.Time, error) {
 	}
 	return dbegin, err
 }
-
-// func (s *appServer) GetMaxDate() (time.Time, error) {
-// 	var dend time.Time
-// 	rqt := "select max(date) from k8sevents;"
-// 	rows, err := s.db.Query(rqt)
-// 	if err != nil {
-// 		return dend, err
-// 	} else {
-// 		defer rows.Close()
-// 		for rows.Next() {
-// 			err = rows.Scan(&dend)
-// 			if err != nil {
-// 				panic(err)
-// 			}
-// 		}
-// 	}
-// 	return dend, err
-// }
 
 func (s *appServer) nbResult(rqt string) (int, error) {
 	var cnt int
@@ -72,7 +55,7 @@ func (s *appServer) nbResult(rqt string) (int, error) {
 
 func (s *appServer) makeRqtEvents(minDateTime time.Time, maxDateTime time.Time, searchName string, typeEvent string, reason string, message string, page int) (string, string) {
 	var limitClause string
-	whereClause := "where firstEventTs between '" + minDateTime.Format("2006-01-02 15:04") + "' and '" + maxDateTime.Format("2006-01-02 15:04") + "'"
+	whereClause := "where firstTime between '" + minDateTime.Format("2006-01-02 15:04") + "' and '" + maxDateTime.Format("2006-01-02 15:04") + "'"
 	if typeEvent != "" {
 		whereClause = whereClause + "and type='" + typeEvent + "'"
 	}
@@ -84,7 +67,7 @@ func (s *appServer) makeRqtEvents(minDateTime time.Time, maxDateTime time.Time, 
 	}
 
 	limitClause = fmt.Sprintf("limit %d offset %d", 50, (page)*50)
-	rqt := "select firstEventTs,eventTs,name,reason,type,message from k8sevents " + whereClause + " order by EventTs,firstEventTs desc " + limitClause
+	rqt := "select exportedTime,firstTime,eventTime,name,reason,type,message from k8sevents " + whereClause + " order by exportedTime desc " + limitClause
 	rqtCnt := "select count(*) from k8sevents " + whereClause
 	return rqt, rqtCnt
 }
@@ -103,7 +86,6 @@ func (s *appServer) calcPages(nbResults int) []string {
 		res = append(res, strconv.Itoa(i))
 	}
 	return res
-
 }
 
 func (s *appServer) IndexHandler(response http.ResponseWriter, request *http.Request) {
@@ -169,9 +151,7 @@ func (s *appServer) IndexHandler(response http.ResponseWriter, request *http.Req
 	tmplt := template.New("index.html")
 	tmplt, _ = tmplt.ParseFiles("./templates/index.html")
 
-	// rqt := "select date,name,reason,type from k8sevents order by date desc limit 50;"
 	rqt, rqtCnt := s.makeRqtEvents(data.Dbegin, data.Dend, request.FormValue("search"), request.FormValue("type"), request.FormValue("reason"), request.FormValue("message"), data.Page)
-	// fmt.Println(rqt)
 
 	nbResults, _ := s.nbResult(rqtCnt)
 	data.NbResults = nbResults
@@ -186,7 +166,7 @@ func (s *appServer) IndexHandler(response http.ResponseWriter, request *http.Req
 		defer rows.Close()
 		for rows.Next() {
 			var rowRes rowK8sevents
-			err = rows.Scan(&rowRes.K8seventFirstTs, &rowRes.K8seventTs, &rowRes.K8seventName, &rowRes.K8seventReason, &rowRes.K8seventType, &rowRes.K8seventMessage)
+			err = rows.Scan(&rowRes.ExportedTime, &rowRes.FirstTime, &rowRes.EventTime, &rowRes.Name, &rowRes.Reason, &rowRes.Type, &rowRes.Message)
 			if err != nil {
 				panic(err)
 			}
